e6b40eef6b58a08f68d248f59f2af352c8a785c9
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-12-01 19:12:29 -0800

    spin up react skeleton file structure for portfolio page. configure webpack, babel transpiler, git, and an index route with Main and Home components.

---
+++ app/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default class Home extends React.Component {
+
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div>
+      	<h1> Portolio Page! </h1>
+        <h2> Why isn't this working? </h2>
+      </div>
+    );
+  }
+}
