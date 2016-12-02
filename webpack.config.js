7eae606dfc7957b393fb30f152df19fce75f3bc2
BenGoldstein88 <bengoldsteinmusic@gmail.com>
2016-12-01 21:32:16 -0800

    more heroku fixes

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,7 @@ module.exports = {
   },
   plugins: [
     HTMLWebpackPluginConfig,
-    new webpack.HotModuleReplacementPlugin(),
+    // new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
     loaders: [
